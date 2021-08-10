5b17c807f0ddaf06ff0dc96f900a2e216540a9d3
tustica <tim.ustica@gmail.com>
2021-08-10 15:29:22 -0500

    first commit

--- users_app/urls.py
+++ users_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('dojos', views.dojos_and_ninjas),
+    path('process-dojo', views.process_dojo),
+    path('process-ninja', views.process_ninja),
     path('process', views.process),
+    path('delete-dojo', views.delete_dojo),
 ]
--- users_app/views.py
+++ users_app/views.py
@@ -1,9 +1,26 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
-from .models import users
+from django.shortcuts import redirect, render
+from .models import dojos, ninjas, users
 
 def index(request):
     return render(request, 'index.html')
+
+def dojos_and_ninjas(request):
+    context = {
+        "all_dojos": dojos.objects.all(),
+        "all_ninjas": ninjas.objects.all()
+    }
+    return render(request, 'dojos_and_ninjas.html', context)
+
+def process_dojo(request):
+    dojos.objects.create(dojo_name = request.POST['dojo_name'],
+    dojo_city = request.POST['dojo_city'], dojo_state = request.POST['dojo_state'])
+    return redirect('/dojos')
+
+def process_ninja(request):
+    ninjas.objects.create(first_name = request.POST['first_name'],
+    last_name = request.POST['last_name'], dojo = dojos.objects.get(id=request.POST['dojo_select']))
+    return redirect('/dojos')
 
 def process(request):
     new_user = users.objects.create(first_name = request.POST['first_name'], 
@@ -12,3 +29,8 @@ def process(request):
         "all_users": users.objects.all()
     }
     return render(request, 'index.html', context)
+
+def delete_dojo(request):
+    deleting = dojos.objects.get(id = request.POST['dojo_delete'])
+    deleting.delete()
+    return redirect('/dojos')
